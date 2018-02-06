d4b441250fad0f7287a894af7355b5f8b999dfa5
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2018-02-05 22:20:01 -0400

    subiendo lo basico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { GoogleMaps } from '@ionic-native/google-maps';
+import { HttpClientModule } from '@angular/common/http';
+import { Proveedor1Provider } from '../providers/proveedor1/proveedor1';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,7 +38,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    GoogleMaps,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Proveedor1Provider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,74 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Proveedor1Provider } from '../../providers/proveedor1/proveedor1';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  map: GoogleMap;
+  mapas
+  constructor(public navCtrl: NavController, private googleMaps: GoogleMaps, public provedor: Proveedor1Provider) {
 
-  constructor(public navCtrl: NavController) {
+  }
+  ionViewDidLoad(){
+    this.provedor.obtenerdatos().subscribe(
+      (data)=>{this.mapas = data;},
+      (error)=> {console.log(error);}
+  )
+  }
+  loadMap(){
 
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904, // default location
+          lng: -89.3809802 // default location
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+  
+    this.map = this.googleMaps.create('map_canvas', mapOptions);
+  
+    // Wait the MAP_READY before using any methods.
+    this.map.one(GoogleMapsEvent.MAP_READY)
+    .then(() => {
+      // Now you can use all methods safely.
+      this.getPosition();
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+  
+  }
+  getPosition(): void{
+    this.map.getMyLocation()
+    .then(response => {
+      this.map.moveCamera({
+        target: response.latLng
+      });
+      this.map.addMarker({
+        title: 'My Position',
+        icon: 'blue',
+        animation: 'DROP',
+        position: response.latLng
+      });
+    })
+    .catch(error =>{
+      console.log(error);
+    });
   }
 
 }
